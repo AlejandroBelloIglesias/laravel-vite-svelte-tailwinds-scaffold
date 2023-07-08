43c3036268e900b65e63dd144bee5977c841d708
Alejandro Bello Iglesias <alejandro.bello@nomasystems.com>
2023-07-08 20:41:45 +0200

    First commit. Working scaffold

---
+++ resources/js/Pages/Index.svelte
@@ -0,0 +1,6 @@
+<script>
+    
+</script>
+ 
+
+<h1 class="block text-blue-700 text-center font-bold text-4xl">Hello World!</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.html',
+    './resources/**/*.svelte',
+    './resources/**/*.php',
+    './resources/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
